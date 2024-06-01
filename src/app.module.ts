7fdc93b73c50230a6025acb08cf94c3c4b51ffaf
Gabriel Spada <gabrielspada0@gmail.com>
2024-06-01 15:17:17 -0300

    Add retry to typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,46 +1,46 @@
-import {Module} from '@nestjs/common';
-import {AppController} from "./default/controllers/app.controller";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {EventEmitterModule} from "@nestjs/event-emitter";
-import {ConfigModule} from "@nestjs/config";
-import {LoggerModule} from "./common/helpers/logger/logger.module";
-import {AuthModule} from "./auth/auth.module";
-import {UserModule} from './user/user.module';
-import {MoviesModule} from './movies/movies.module';
-import {RedisModule} from "@nestjs-modules/ioredis";
-import {CacheModuleClass} from "./cache/cache.module";
+import { Module } from "@nestjs/common";
+import { AppController } from "./default/controllers/app.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { EventEmitterModule } from "@nestjs/event-emitter";
+import { ConfigModule } from "@nestjs/config";
+import { LoggerModule } from "./common/helpers/logger/logger.module";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { MoviesModule } from "./movies/movies.module";
+import { RedisModule } from "@nestjs-modules/ioredis";
+import { CacheModuleClass } from "./cache/cache.module";
 
 
 @Module({
     imports: [
         ConfigModule.forRoot({
-            isGlobal: true,
+            isGlobal: true
         }),
         RedisModule.forRootAsync({
             useFactory: () => ({
-                type: 'single',
+                type: "single",
                 url: process.env.REDIS_URL,
                 options: {
-                    password: process.env.REDIS_PASSWORD,
+                    password: process.env.REDIS_PASSWORD
                 },
                 ttl: 300,
                 reconnectOnError: false,
                 retryStrategy: false
-            }),
+            })
         }),
         TypeOrmModule.forRoot({
-            type: 'postgres',
+            type: "postgres",
             host: process.env.DB_HOST,
             port: Number(process.env.DB_PORT),
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
-            entities: ['dist/**/*.entity{.ts,.js}'],
+            entities: ["dist/**/*.entity{.ts,.js}"],
             synchronize: true,
             logging: false,
             // logger: 'file',
-            retryAttempts: 1,
-            retryDelay: 5000
+            retryAttempts: 10,
+            retryDelay: 10000
         }),
         EventEmitterModule.forRoot({
             ignoreErrors: true,
@@ -52,7 +52,7 @@ import {CacheModuleClass} from "./cache/cache.module";
         LoggerModule,
         MoviesModule,
         CacheModuleClass],
-    controllers: [AppController],
+    controllers: [AppController]
 
 })
 export class AppModule {
